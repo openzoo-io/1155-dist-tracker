b2df596566a6fb5eb05caf69b19174846f4e11fb
jason <kwon_jason@yahoo.com>
2021-05-19 22:37:35 +0300

    1155 dist tracker

---
+++ services/erc1155tracker.js
@@ -0,0 +1,198 @@
+require('dotenv').config()
+const ethers = require('ethers')
+const mongoose = require('mongoose')
+const ERC1155CONTRACT = mongoose.model('ERC1155CONTRACT')
+const ERC1155TOKEN = mongoose.model('ERC1155TOKEN')
+
+const SimplifiedERC1155ABI = require('../constants/simplified1155abi')
+
+const provider = new ethers.providers.JsonRpcProvider(
+  process.env.MAINNET_RPC,
+  parseInt(process.env.MAINNET_CHAINID),
+)
+
+const toLowerCase = (val) => {
+  if (val) return val.toLowerCase()
+  else return val
+}
+
+const validatorAddress = '0x0000000000000000000000000000000000000000'
+// store trackedAddresses
+const trackedAddresses = []
+const trackedSCs = []
+
+const trackNewERC1155 = async () => {
+  const func = async () => {
+    try {
+      let untrackedSCs = await ERC1155CONTRACT.find({
+        address: { $nin: trackedAddresses },
+      })
+      if (untrackedSCs) {
+        // when there are some untracked addresses
+        let promise = untrackedSCs.map(async (sc) => {
+          let address = sc.address
+          trackedAddresses.push(address)
+          let abi = SimplifiedERC1155ABI
+          let contract = new ethers.Contract(address, abi, provider)
+          trackedSCs.push(contract)
+          // register tracker here
+          contract.on(
+            'TransferSingle',
+            async (operator, from, to, id, value) => {
+              console.log('transfer single')
+              operator = toLowerCase(operator)
+              from = toLowerCase(from)
+              to = toLowerCase(to)
+              id = parseFloat(id.toString())
+              value = parseFloat(value.toString())
+              try {
+                if (from == validatorAddress) {
+                  // this is a new mint
+                  let tk = await ERC1155TOKEN.findOne({ tokenID: id })
+                  if (!tk) {
+                    let newTk = new ERC1155TOKEN()
+                    let ownerMap = new Map()
+                    ownerMap.set(to, value)
+                    newTk.owner = ownerMap
+                    newTk.contractAddress = address
+                    newTk.tokenID = id
+                    newTk.supply = value
+                    newTk.tokenURI = 'https://'
+                    await newTk.save()
+                  }
+                } else {
+                  let tk = await ERC1155TOKEN.findOne({
+                    contractAddress: operator,
+                    tokenID: id,
+                  })
+                  if (tk) {
+                    //add value to the receiver
+                    let ownerMap = tk.owner
+                    if (ownerMap) {
+                      try {
+                        let senderValue = ownerMap.get(from)
+                        let remainingBalance = parseFloat(senderValue) - value
+                        if (remainingBalance == 0) {
+                          ownerMap.delete(from)
+                        } else {
+                          ownerMap.set(from, remainingBalance)
+                        }
+                        //deduct value from sender
+                        let receiverValue = ownerMap.get(to)
+                        receiverValue = receiverValue + value
+                        ownerMap.set(to, receiverValue)
+                        tk.owner = ownerMap
+                        await tk.save()
+                      } catch (error) {
+                        console.log('in save while there is a tk')
+                        console.log(error)
+                      }
+                    } else {
+                      console.log('else')
+                    }
+                  }
+                }
+              } catch (error) {
+                console.log('overall handle single transfer function')
+                console.log(error)
+              }
+            },
+          )
+          contract.on(
+            'TransferBatch',
+            async (operator, from, to, ids, values) => {
+              let promises = ids.map(async (_, index) => {
+                operator = toLowerCase(operator)
+                from = toLowerCase(from)
+                to = toLowerCase(to)
+                let id = ids[index]
+                id = parseFloat(id.toString())
+                let value = values[index]
+                value = parseFloat(value.toString())
+                try {
+                  if (from == validatorAddress) {
+                    let tk = await ERC1155TOKEN.findOne({ tokenID: id })
+                    if (!tk) {
+                      let newTk = new ERC1155TOKEN()
+                      let ownerMap = new Map()
+                      ownerMap.set(to, value)
+                      newTk.owner = ownerMap
+                      newTk.contractAddress = address
+                      newTk.tokenID = id
+                      newTk.supply = value
+                      newTk.tokenURI = 'https://'
+                      await newTk.save()
+                    }
+                  } else {
+                    let tk = await ERC1155TOKEN.findOne({
+                      contractAddress: operator,
+                      tokenID: id,
+                    })
+                    if (tk) {
+                      //add value to the receiver
+                      let ownerMap = tk.owner
+                      if (ownerMap) {
+                        try {
+                          let senderValue = ownerMap.get(from)
+                          let remainingBalance = parseFloat(senderValue) - value
+                          if (remainingBalance == 0) {
+                            ownerMap.delete(from)
+                          } else {
+                            ownerMap.set(from, remainingBalance)
+                          }
+                          //deduct value from sender
+                          let receiverValue = ownerMap.get(to)
+                          receiverValue = receiverValue + value
+                          ownerMap.set(to, receiverValue)
+                          tk.owner = ownerMap
+                          await tk.save()
+                        } catch (error) {
+                          console.log('in save while there is a tk')
+                          console.log(error)
+                        }
+                      } else {
+                        console.log('else')
+                      }
+                    }
+                  }
+                } catch (error) {
+                  console.log('overall handle single transfer function')
+                  console.log(error)
+                }
+                await handleSingleTransfer(
+                  operator,
+                  from,
+                  to,
+                  ids[index],
+                  values[index],
+                  contract,
+                )
+              })
+              Promise.all(promises)
+            },
+          )
+          contract.on('URI', async (value, id) => {
+            console.log('uri 1')
+            setTimeout(async () => {
+              id = parseFloat(id.toString())
+              let tk = await ERC1155TOKEN.findOne({ tokenID: id })
+              let _tkURI = tk.tokenURI
+              if (_tkURI == 'https://') tk.tokenURI = value
+              await tk.save()
+            }, 1000)
+          })
+        })
+        await Promise.all(promise)
+      }
+      setTimeout(async () => {
+        await func()
+      }, 1000 * 10)
+    } catch (error) {
+      console.log('error in tracking new 1155')
+      console.log(error)
+    }
+  }
+  await func()
+}
+
+module.exports = trackNewERC1155
---
+++ services/track_dist_1155.js
@@ -0,0 +1,255 @@
+require("dotenv").config();
+const ethers = require("ethers");
+const mongoose = require("mongoose");
+const ERC1155CONTRACT = mongoose.model("ERC1155CONTRACT");
+const ERC1155TOKEN = mongoose.model("ERC1155TOKEN");
+
+const SimplifiedERC1155ABI = require("../constants/simplified1155abi");
+
+const provider = new ethers.providers.JsonRpcProvider(
+  process.env.MAINNET_RPC,
+  parseInt(process.env.MAINNET_CHAINID)
+);
+
+const toLowerCase = (val) => {
+  if (val) return val.toLowerCase();
+  else return val;
+};
+
+// xtract address from ST Evt topics
+const extractAddress = (data) => {
+  let length = data.length;
+  return data.substring(0, 2) + data.substring(length - 40);
+};
+
+// get tokenID & total supply from data
+const parseSingleTrasferData = (data) => {
+  return [
+    parseInt(data.substring(0, 66), 16),
+    parseInt(data.substring(66), 16),
+  ];
+};
+const parseBatchTransferData = (data) => {
+  let tokenIDs = [];
+  data = data.substring(2);
+  let segments = data.length / 64;
+  let tkCount = segments / 2;
+  let tkData = data.substring(64 * 3, 64 * (tkCount + 1));
+  for (let i = 0; i < tkData.length / 64; ++i) {
+    let _tkData = tkData.substring(i * 64, (i + 1) * 64);
+    let tokenID = parseInt(_tkData.toString(), 16);
+    tokenIDs.push(tokenID);
+  }
+
+  return tokenIDs;
+};
+
+const getSupplyPerAddress = async (tokenID, address, contract) => {
+  let supply = await contract.balanceOf(address, parseInt(tokenID));
+  return supply;
+};
+
+const parseURIHexToString = (data) => {
+  let utf8String = ethers.utils.toUtf8String(
+    data.substring(0, 2) + data.substring(130)
+  );
+  return utf8String;
+};
+
+const validatorAddress = "0x0000000000000000000000000000000000000000";
+// store trackedAddresses
+const trackedAddresses = [];
+const trackedSCs = [];
+
+const analyzeEvents = async (address, contract) => {
+  /* map for total supplies per tokenID*/
+  const supplies = new Map(); //  tokenID => total supply
+  /* map for token uri per tokenID*/
+  const tokenUris = new Map(); //  tokenID => tokenURI
+  /* map for holders per tokenID*/
+  const holders = new Map(); //tokenID => address[]
+
+  let cbh = await provider.getBlockNumber(); //current block height
+
+  // get all single transfer events
+  let ste = await provider.getLogs({
+    address: address,
+    fromBlock: 0,
+    toBlock: cbh,
+    topics: [
+      ethers.utils.id(
+        "TransferSingle(address,address,address,uint256,uint256)"
+      ),
+    ],
+  });
+  // get all URI events
+  let urie = await provider.getLogs({
+    address: address,
+    fromBlock: 0,
+    toBlock: cbh,
+    topics: [ethers.utils.id("URI(string,uint256)")],
+  });
+  // get all batch transfer events
+  let bte = await provider.getLogs({
+    address: address,
+    fromBlock: 0,
+    toBlock: cbh,
+    topics: [
+      ethers.utils.id(
+        "TransferBatch(address,address,address,uint256[],uint256[])"
+      ),
+    ],
+  });
+
+  //   loop single transfer events first along with uri event
+  for (let i = 0; i < ste.length; ++i) {
+    //loop singe transfer evt
+    let singleTransferEvt = ste[i];
+    let uriEvt = urie[i];
+    let stTopics = singleTransferEvt.topics;
+    let stData = singleTransferEvt.data;
+    let uriData = uriEvt.data;
+    let sender = extractAddress(stTopics[2]);
+    sender = toLowerCase(sender);
+    let receiver = extractAddress(stTopics[3]);
+    receiver = toLowerCase(receiver);
+
+    stData = parseSingleTrasferData(stData);
+    let tokenID = stData[0];
+    let supply = stData[1];
+    let tokenURI = parseURIHexToString(uriData);
+
+    // when this is a mint evt, log total supply & token uri
+    if (sender == validatorAddress) {
+      supplies.set(tokenID, supply);
+      tokenUris.set(tokenID, tokenURI);
+    }
+    /* add unseen addresses per tokenID */
+    let holdersPerTkID = holders.get(tokenID);
+    if (holdersPerTkID) {
+      // when there is a registered array for a particular tokenID
+      if (!holdersPerTkID.includes(sender))
+        if (sender != validatorAddress) holdersPerTkID.push(sender);
+      if (!holdersPerTkID.includes(receiver)) holdersPerTkID.push(receiver);
+      holders.set(tokenID, holdersPerTkID);
+    } else {
+      let _holdersPerTkID = [];
+      if (sender != validatorAddress) _holdersPerTkID.push(sender);
+      _holdersPerTkID.push(receiver);
+      holders.set(tokenID, _holdersPerTkID);
+    }
+  } //loop singe transfer evt
+
+  /* now loop the batch transfer event */
+  for (let i = 0; i < bte.length; ++i) {
+    let batchTransferEvt = bte[i];
+    let bteTopics = batchTransferEvt.topics;
+    let bteData = batchTransferEvt.data;
+    let sender = bteTopics[2];
+    let receiver = bteTopics[3];
+    sender = toLowerCase(extractAddress(sender)); // sender
+    receiver = toLowerCase(extractAddress(receiver)); // receiver
+    let ids = parseBatchTransferData(bteData);
+    ids.map((tokenID) => {
+      /* add unseen addresses per tokenID */
+      let holdersPerTkID = holders.get(tokenID);
+      if (holdersPerTkID) {
+        // when there is a registered array for a particular tokenID
+        if (!holdersPerTkID.includes(sender))
+          if (sender != validatorAddress)
+            holdersPerTkID.push(toLowerCase(sender));
+        if (!holdersPerTkID.includes(receiver))
+          holdersPerTkID.push(toLowerCase(receiver));
+        holders.set(tokenID, holdersPerTkID);
+      } else {
+        let _holdersPerTkID = [];
+        if (sender != validatorAddress) _holdersPerTkID.push(sender);
+        _holdersPerTkID.push(receiver);
+        holders.set(tokenID, _holdersPerTkID);
+      }
+    });
+    /* extract tokenIDs & corrensponding transfered values from data */
+  }
+
+  let tkIDs = holders.keys();
+  let tkID = tkIDs.next().value;
+  while (tkID) {
+    let holderAddrs = holders.get(tkID);
+    let _savedTk = await ERC1155TOKEN.findOne({
+      contractAddress: address,
+      tokenID: tkID,
+    });
+    let ownerMap = new Map();
+    if (_savedTk) {
+      let owners = _savedTk.owner;
+      let ownerAddrs = owners.keys();
+      let _ownerAddr = ownerAddrs.next().value;
+      while (_ownerAddr) {
+        if (!holderAddrs.includes(_ownerAddr)) holderAddrs.push(_ownerAddr);
+      }
+      let promise = holderAddrs.map(async (holderAddr) => {
+        let supply = await getSupplyPerAddress(tkID, holderAddr, contract);
+        supply = parseInt(supply.toString());
+        ownerMap.set(holderAddr, supply);
+      });
+      await Promise.all(promise);
+      //   now save a token
+      _savedTk.tokenURI = tokenUris.get(tkID);
+      _savedTk.owner = ownerMap;
+      _savedTk.supply = supplies.get(tkID);
+
+      try {
+        await _savedTk.save();
+      } catch (error) {}
+
+      tkID = tkIDs.next().value;
+    } else {
+      let promise = holderAddrs.map(async (holderAddr) => {
+        let supply = await getSupplyPerAddress(tkID, holderAddr, contract);
+        supply = parseInt(supply.toString());
+        ownerMap.set(holderAddr, supply);
+      });
+      await Promise.all(promise);
+      let savingTk = new ERC1155TOKEN();
+      savingTk.contractAddress = address;
+      savingTk.tokenID = tkID;
+      savingTk.tokenURI = tokenUris.get(tkID);
+      savingTk.symbol = "symbol";
+      savingTk.owner = ownerMap;
+      savingTk.supply = supplies.get(tkID);
+      try {
+        await savingTk.save();
+      } catch (error) {}
+
+      tkID = tkIDs.next().value;
+    }
+  }
+};
+
+const track1155Distribution = async () => {
+  //track 1155 dist
+  const func = async () => {
+    try {
+      let untrackedSCs = await ERC1155CONTRACT.find({
+        address: { $nin: trackedAddresses },
+      });
+      if (untrackedSCs) {
+        let promise = untrackedSCs.map(async (sc) => {
+          let address = sc.address;
+          trackedAddresses.push(address);
+          let abi = SimplifiedERC1155ABI;
+          let contract = new ethers.Contract(address, abi, provider);
+          trackedSCs.push(contract);
+          await analyzeEvents(address, contract);
+        });
+      }
+    } catch (error) {}
+
+    setTimeout(async () => {
+      await func();
+    }, 1000 * 60 * 10); //repeat every 10 secs for now
+  };
+  await func();
+}; //track 1155 dist
+
+module.exports = track1155Distribution;
